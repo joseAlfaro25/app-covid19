0c459a6765624af1a6d07cddfbf41a876ecdcf85
joseAlfaro25 <43280997+joseAlfaro25@users.noreply.github.com>
2020-09-23 12:49:26 -0500

    Login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,14 +2,15 @@
 import app from '../../services/auth/base'
 import React, { useCallback, useContext, Fragment } from "react";
 import { withRouter, Redirect } from "react-router";
-import {AuthContext} from "../../services/auth/Auth";
+import { AuthContext } from "../../services/auth/Auth";
+import './Login.css';
 
 
 
 
 const Login = ({ history }) => {
     const handleLogin = useCallback(
-        async event => {
+        async (event) => {
             event.preventDefault();
             const { email, password } = event.target.elements;
             try {
@@ -32,20 +33,41 @@ const Login = ({ history }) => {
 
     return (
         <div>
+            <div className="container">
+                <div className="abs-center">
+                    <div className="jumbotron col-md-5">
+                        <h3>Iniciar sesión</h3>
+                        <form onSubmit={handleLogin}>
 
-            
-            <h1>Log in</h1>
-            <form onSubmit={handleLogin}>
-                <label>
-                    Email
-          <input name="email" type="email" placeholder="Email" />
-                </label>
-                <label>
-                    Password
-          <input name="password" type="password" placeholder="Password" />
-                </label>
-                <button type="submit">Log in</button>
-            </form>
+
+                            <div className="form-group">
+                                <div className="input-icon icon-username"></div>
+                                <input
+                                    name="email"
+                                    type="email"
+                                    className="form-control mb-4"
+                                    placeholder="Correo"
+                                    required
+                                />
+                            </div>
+                            <div className="form-group">
+                                <div className="input-icon icon-password"></div>
+                                <input
+                                    name="password"
+                                    type="password"
+                                    className="form-control mb-4"
+                                    placeholder="Contraseña"
+                                />
+                            </div>
+                            <button type="submit" className="btn btn-primary btn-lg btn-block">
+                                Entrar
+                <span></span>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/components/model/DataPeople.js
+++ src/components/model/DataPeople.js
@@ -1,25 +1,56 @@
-import React, { } from 'react'
-
+import React, { useState, useEffect } from 'react'
+import app  from '../../services/auth/base'
 
 const DataPeople = () => {
-    const [tareas, setTareas] = React.useState([])
+    const [datos, setDatos] = useState([])
 
-    React.useEffect(() => {
+    useEffect(() => {
 
         const obtenerDatos = async () => {
-            const db = app.firestore()
-            try {
+      let db =  app.firestore()
+          
                 const data = await db.collection('datos').get()
                 const arrayData = data.docs.map(doc => ({ id: doc.id, ...doc.data() }))
                 console.log(arrayData)
-                setTareas(arrayData)
-            } catch (error) {
+                setDatos(arrayData)
                 console.log(error)
-            }
+            
         }
         obtenerDatos()
 
     }, [])
+
+    const pacientes = datos.map((data, i) => {
+
+
+        return (
+
+
+
+            <div
+                lat={parseFloat(data.latitud)}
+                lng={parseFloat(data.longitud)}
+                style={{
+                    color: "black",
+                    backgroundColor: "white",
+                    height: "25px",
+                    width: "35px",
+
+
+                }
+                }
+
+
+            >
+                <img height="10px" alt="10" src={data.countryInfo.flag} >
+                </img>
+
+                {data.nombre}
+            </div>
+
+
+        )
+    })
     return (
         <Fragment>
 
